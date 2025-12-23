b424d742212c553ebb2d3640c810f2a256bfe53d
nylinUA <nylin@ualberta.ca>
2025-12-22 21:13:03 -0700

    Add partial autoclicker

---
+++ app/src/main/java/com/example/macaronclicker/AutoClicker.java
@@ -0,0 +1,18 @@
+package com.example.macaronclicker;
+
+import android.view.View;
+import android.widget.TextView;
+
+public class AutoClicker {
+
+    public void autoclick(){
+
+    }
+
+    public interface autoClicker {
+        void incrementClickCount();
+    }
+
+
+
+}
--- app/src/main/java/com/example/macaronclicker/MainActivity.java
+++ app/src/main/java/com/example/macaronclicker/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.macaronclicker;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,11 +14,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity{
 
     private ImageView macaron;
-    private TextView clicks_view;
-    private int num_clicks;
+    private TextView clicksView;
+    private TextView autoClickerView;
+    private int numClicks;
+    private Boolean autoClickerOn = false;
+    private Timer timer;
+    private TimerTask task;
+    private int timerPeriod;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,21 +41,64 @@ public class MainActivity extends AppCompatActivity{
             return insets;
         });
 
-        num_clicks = 0;
-        clicks_view = findViewById(R.id.clicks);
+        numClicks = 0;
+        clicksView = findViewById(R.id.clicks);
+
         macaron = findViewById(R.id.macaron);
         MyPressListener myPressListener = new MyPressListener();
         macaron.setOnTouchListener(myPressListener);
         macaron.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                num_clicks += 1;
-                clicks_view.setText(String.valueOf(num_clicks));
+                incrementClickCount();
+            }
+        });
+
+        timer = new Timer();
+
+        autoClickerView = findViewById(R.id.auto_clicker);
+        autoClickerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (autoClickerOn){
+                    task.cancel();
+                    timer.cancel();
+                    autoClickerOn = false;
+                    autoClickerView.setText("AutoClicker");
+                    autoClickerView.setTextColor(Color.parseColor("#1f2326"));
+                }
+                else{
+                    timer = new Timer();
+                    task = new TimerTask() {
+                        @Override
+                        public void run() {
+                            incrementClickCount();
+                        }
+                    };
+                    timer.schedule(task, 0, 2000);
+                    autoClickerOn = true;
+                    autoClickerView.setText("0.33 per second");
+                    autoClickerView.setTextColor(Color.parseColor("#fdacc8"));
+                }
             }
         });
 
 
 
+
+
+
+
+    }
+
+    public void incrementClickCount(){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                numClicks += 1;
+                clicksView.setText(String.valueOf(numClicks));
+            }
+        });
 
     }
 
